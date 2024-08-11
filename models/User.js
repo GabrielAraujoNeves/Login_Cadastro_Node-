2027920b1048c3057cbd14c663d63927d967a56e
GabrielNeves <eg69228@gmail.com>
2024-08-11 02:29:26 -0400

    Add user registration, login, and profile update endpoints with validation

--- models/User.js
+++ models/User.js
@@ -1,9 +1,13 @@
 const mongoose = require('mongoose');
 
 const UserSchema = new mongoose.Schema({
-    name: {type: String, required: true},
-    email: {type: String, required: true, unique: true },
-    password: {type: String, require: true},
+    name: { type: String, required: true},
+    email: { type: String, required: true, unique: true },
+    password: { type: String, require: true},
+    age: { type: Number, required: true},
+    location: { type: String, required: true},
+    profilePicture: { type: Buffer},
+    description: { type: String},
 });
 
 const User = mongoose.model('User', UserSchema);
--- src/routes.js
+++ src/routes.js
@@ -1,96 +1,162 @@
-const express =  require('express');
+const express = require('express');
 const Router = express.Router();
-const {check, validationResult} = require('express-validator');
+const { check, validationResult } = require('express-validator');
 const User = require('../models/User');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const auth = require('../src/middleware/auth');
+const upload = require('../src/middleware/upload');
 
-
-Router.get('/users', auth, async(req , res) => {
-       try{
+// Obter todos os usuários (apenas administradores)
+Router.get('/users', auth, async (req, res) => {
+    try {
         const users = await User.find();
         res.status(200).json(users);
-       } catch (error) {
-        res.status(500).json({ error: error.message})
-       }
-    });
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+});
 
-
+// Registrar um novo usuário
 Router.post('/register', [
+    upload.single('profilePicture'),
+    check('name').notEmpty().withMessage('Name is required'),
+    check('email').isEmail().withMessage('Invalid email'),
+    check('password').isLength({ min: 6 }).withMessage('Password must be at least 6 characters long')
+], async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+    }
 
-      check('name').notEmpty().withMessage('Name is required'),
-      check('email').isEmail().withMessage('Invalid email'),
-      check('password').isLength({min: 6}).withMessage('Password must be at laest 6 characters long')
-
-      ], async (req, res) => {
-
-      const errors = validationResult(req);
-
-      if(!errors.isEmpty()){
-         return res.status(400).json({ errors: errors.array() });
-      }
-
-      const {name, email, password} = req.body;
-      try{
+    const { name, email, password, age, location, description } = req.body;
+    try {
         const existingUser = await User.findOne({ email });
-        if (existingUser){
-            return res.status(400).json({ error: 'Email already in use'});
+        if (existingUser) {
+            return res.status(400).json({ error: 'Email already in use' });
         }
 
         const hashedPassword = await bcrypt.hash(password, 10);
-        const newUser = new User({ name, email, password: hashedPassword});
+        const newUser = new User({
+            name,
+            email,
+            password: hashedPassword,
+            age,
+            location,
+            description,
+            profilePicture: req.file ? req.file.buffer : undefined
+        });
         await newUser.save();
 
         res.status(201).json(newUser);
-      } catch (error) {
+    } catch (error) {
         res.status(500).json({ error: error.message });
-   }
+    }
 });
 
-
-Router.post('/login',[
+// Login do usuário
+Router.post('/login', [
     check('email').isEmail().withMessage('Invalid email'),
-    check('password').notEmpty().withMessage('Password is require')
-    ], async (req, res) => {
-       
-        const errors = validationResult(req);
+    check('password').notEmpty().withMessage('Password is required')
+], async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+    }
 
-        if(!errors.isEmpty){
-            return res.status(400).json({ errors: errors.array() });
-        }
-       
-       const { email, password} = req.body;
-       try{
-
+    const { email, password } = req.body;
+    try {
         const user = await User.findOne({ email });
-        if(!user) {
-            return res.status(400).json({ error: 'Invalid email  or password'});
+        if (!user) {
+            return res.status(400).json({ error: 'Invalid email or password' });
         }
 
         const isMatch = await bcrypt.compare(password, user.password);
         if (!isMatch) {
-            return res.status(400).json({ error: 'Invalid email or password'});
+            return res.status(400).json({ error: 'Invalid email or password' });
         }
 
-        const token = jwt.sign({ userId: user._id}, process.env.JWT_SECRET, { expiresIn: '1h'});
+        const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET, { expiresIn: '1h' });
 
         res.status(200).json({ token });
-    }catch (error) {
-        res.status(500).json({ error: error.message});
+    } catch (error) {
+        res.status(500).json({ error: error.message });
     }
 });
 
+// Deletar um usuário por ID
 Router.delete('/users/:id', auth, async (req, res) => {
     const { id } = req.params;
-    try{
+    try {
         const user = await User.findByIdAndDelete(id);
-        if(!user) {
-            return res.status(404).json({ error: 'user not found'});
+        if (!user) {
+            return res.status(404).json({ error: 'User not found' });
         }
-        res.status(200).json({ message: 'User deleted successfully'});
-    } catch  (error){
-        res.status(500).json({ error: error.message});
+        res.status(200).json({ message: 'User deleted successfully' });
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+});
+
+// Atualizar o perfil do usuário autenticado
+Router.patch('/profile', [
+    auth,
+    upload.single('profilePicture'),
+    check('name').notEmpty().withMessage('Name is required'),
+    check('age').isInt({ min: 0 }).withMessage('Invalid age'),
+    check('location').notEmpty().withMessage('Location is required'),
+    check('description').notEmpty().withMessage('Description is required')
+], async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+    }
+
+    const { name, age, location, description } = req.body;
+    const userId = req.user.id;  // ID do usuário autenticado
+
+    try {
+        const updateFields = {
+            name,
+            age,
+            location,
+            description,
+        };
+
+        if (req.file) {
+            updateFields.profilePicture = req.file.buffer;  // Atualiza a imagem do perfil se um arquivo foi enviado
+        }
+
+        const updatedUser = await User.findByIdAndUpdate(
+            userId,
+            updateFields,
+            { new: true }
+        );
+
+        if (!updatedUser) {
+            return res.status(404).json({ error: 'User not found' });
+        }
+
+        res.status(200).json(updatedUser);
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+});
+
+// Obter o perfil do usuário autenticado
+Router.get('/profile', auth, async (req, res) => {
+    const userId = req.user.id;  // ID do usuário autenticado
+
+    try {
+        const user = await User.findById(userId);
+
+        if (!user) {
+            return res.status(404).json({ error: 'User not found' });
+        }
+
+        res.status(200).json(user);
+    } catch (error) {
+        res.status(500).json({ error: error.message });
     }
 });
 
